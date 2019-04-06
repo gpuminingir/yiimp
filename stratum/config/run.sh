728a3925a01f2fb8e825ca7d2c336fb702f90ad6
gpuminingir <45966078+gpuminingir@users.noreply.github.com>
2019-04-06 11:07:40 +0100

    latest

--- stratum/config/run.sh
+++ stratum/config/run.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
 
+#!/bin/bash
 ulimit -n 10240
 ulimit -u 10240
-
 cd /var/stratum
-while [ -e config/${1}.conf ]; do
-	gzip -f config/${1}.log
-        ./stratum config/$1
-	sleep 1
+while true; do
+./stratum /var/stratum/config/$1
+sleep 2
 done
 exec bash
 
